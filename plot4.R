0172af8d09c297c6e02d578b5597fe6c8bc401ce
pdiazs <pdiazs@ono.com>
2014-05-07 17:50:25 +0200

    Update plot4.R

--- plot4.R
+++ plot4.R
@@ -1,4 +1,11 @@
-png('plot4.png',width = 480, height = 480)
+## Read Data and review
+## The script assumes G:/R/Coursera Explora/ as path. Change and adapt to your needs
+household_power_consumption <- read.csv("G:/R/Coursera Explora/exdata_data_household_power_consumption/household_power_consumption.txt", sep=";")
+View(household_power_consumption)
+str(household_power_consumption)
+##Transform Data into adequate classespng('plot4.png',width = 480, height = 480)
+
+
 par(mfrow=c(2,2))
 plot(sub.hpcs$Global_active_power, type="l",ylab="Global Active Power (kilowatts)", xlab="", xaxt = 'n')
 axis(1, at=c(1,1440,2880), labels=rot[1:3])
